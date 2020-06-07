ce5dd08b864f599b57a135309554f56b07d2ac8c
Tanveer <tanveersodhi17@gmail.com>
2020-06-07 15:22:10 +0530

    Redux Add Favorite

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -4,13 +4,19 @@ import { Card, Icon  } from 'react-native-elements';
 
 import {connect} from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
         dishes: state.dishes,
-        comments: state.comments
+        comments: state.comments,
+        favorites: state.favorites
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId))
+});
 
 function RenderDish(props) {
 
@@ -68,17 +74,10 @@ function RenderComments(props) {
 
 class Dishdetail extends Component {
 
-    constructor(props){
-        super(props);
-        this.state ={
-            
-            favorites: []
-        };
-    }
+    
 
     markFavorite(dishId){
-        this.setState({favorites: this.state.favorites.concat(dishId)})
-    }
+        this.props.postFavorite(dishId);    }
 
    
 
@@ -88,7 +87,7 @@ class Dishdetail extends Component {
         return(
             <ScrollView>
                 <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                    favorite={this.state.favorites.some(el => el===dishId)}
+                    favorite={this.props.favorites.some(el => el===dishId)}
                     onPress={() => this.markFavorite(dishId)}
                 />
                 <RenderComments comments={this.props.comments.comments.filter((comment)=> comment.dishId===dishId)}/>
@@ -98,4 +97,4 @@ class Dishdetail extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Dishdetail);
+export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
--- redux/ActionTypes.js
+++ redux/ActionTypes.js
@@ -1,3 +1,5 @@
+import { exp } from "react-native-reanimated";
+
 export const DISHES_LOADING = 'DISHES_LOADING';
 export const ADD_DISHES = 'ADD_DISHES';
 export const DISHES_FAILED = 'DISHES_FAILED';
@@ -9,3 +11,5 @@ export const PROMOS_FAILED = 'PROMOS_FAILED';
 export const LEADERS_LOADING = 'LEADERS_LOADING';
 export const ADD_LEADERS = 'ADD_LEADERS';
 export const LEADERS_FAILED = 'LEADERS_FAILED';
+export const POST_FAVOURITE = 'POST_FAVOURITE';
+export const ADD_FAVORITE = 'ADD_FAVORITE';
