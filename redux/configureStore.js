9bc6468750d90ab0791944154d85474c48f9a106
Tanveer <tanveersodhi17@gmail.com>
2020-06-09 19:50:41 +0530

    Persist Redux Store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { dishes } from './dishes';
@@ -6,10 +6,22 @@ import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
+
+const config = {
+
+key: 'root',
+
+storage: AsyncStorage,
+
+debug: true
+
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             dishes,
             comments,
             promotions,
@@ -18,6 +30,6 @@ export const ConfigureStore = () => {
         }),
         applyMiddleware(thunk, logger)
     );
-
-    return store;
+    const persistor = persistStore(store);
+    return {persistor,store};
 }
